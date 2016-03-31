5dcdb161909b81d737d095bc9d6f2e95ef1605b3
Anish Pavuluri <anish.pavuluri@gmail.com>
2016-03-31 23:41:30 +0530

    mail changes

--- src/main/java/com/village/rajavolu/Util/DaoTest.java
+++ src/main/java/com/village/rajavolu/Util/DaoTest.java
@@ -2,6 +2,8 @@ package com.village.rajavolu.util;
 
 import com.village.rajavolu.dao.UserDao;
 import com.village.rajavolu.dto.User;
+import com.village.rajavolu.enums.TemplateNames;
+import com.village.rajavolu.mail.MailService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,14 +19,17 @@ public class DaoTest {
 
     public static void main(String args[]) {
 
-        /*ApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
-        UserDao userDao = (UserDao) context.getBean("userDao");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
+        //UserDao userDao = (UserDao) context.getBean("userDao");
         //List<User> userList =userDao.findByUserEmailAndPassword("vsv.valluru@gmail.com", "qwe123");
-        List<User> userList =userDao.findByUserEmail("9491452162");
+        /*List<User> userList =userDao.findByUserEmail("9491452162");
         System.out.println("#######user##"+userList);
         for(User user : userList){
             System.out.println("#################"+user.getEmailId() + " --- "+ user.getMobileNo());
         }*/
+        MailService mailService = (MailService) context.getBean("mailService");
+        mailService.sendMail("anish.pavuluri@gmail.com", null, TemplateNames.templates.getPath()+ TemplateNames.registrationMail.name(), "HI");
+        System.out.println("############ SUCCESS ##########");
 
     }
 }
